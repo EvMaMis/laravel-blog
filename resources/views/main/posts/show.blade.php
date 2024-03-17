b24f418d4871974a207843b092911dbc1485dc00
EvMaMis <evmamis13@gmail.com>
2024-03-17 13:18:02 +0200

    fixed single page alignment

--- resources/views/main/posts/show.blade.php
+++ resources/views/main/posts/show.blade.php
@@ -43,15 +43,16 @@
 
 @section('content')
     <article class="mb-4">
-        <div class="container px-4 px-lg-5">
-            <div class="row gx-4 gx-lg-5 justify-content-center">
-                <div class="col-md-10 col-lg-8 col-xl-7">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-10">
                     {!! $post->content !!}
                 </div>
             </div>
             @if(sizeof($relatedPosts) != 0)
                 <div class="section-heading mb-3 text-center">Похожие посты</div>
-                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 justify-content-center">
+                <div class="row justify-content-center">
+                <div class="col-10 row row-cols-1 row-cols-sm-2 row-cols-md-3 justify-content-center">
                     @foreach($relatedPosts as $relatedPost)
                         <div class="col post">
                             <div class="user-post-image">
@@ -69,25 +70,30 @@
                         </div>
                     @endforeach
                 </div>
-            @endif
+                </div>
 
+            @endif
             <div class="row row-cols-1 justify-content-center">
-                <div class="section-heading mb-3 text-center">Комментарии({{$post->comments->count()}})</div>
-                @foreach($post->comments as $comment)
-            <div class="col-8 card-comment mb-3">
-                <div class="comment-text">
-                <div class="username">
-                    {{$comment->user->name}}
-                <span class="text-muted float-right">{{$comment->dateAsCarbon->diffForHumans()}}</span>
-                </div>
-                    {{$comment->message}}
-                </div>
-            </div>
-                @endforeach
+                <div class="section-heading mb-3 text-center">Комментарии{{$post->comments->count() != 0 ? "(" . $post->comments->count() . ")" : '' }}</div>
+                @if($post->comments->count() == 0)
+                    <div class="col-10 h2">Здесь пока нет комментариев</div>
+                @else
+                    @foreach($post->comments as $comment)
+                        <div class="col-8 card-comment mb-3">
+                            <div class="comment-text">
+                                <div class="username">
+                                    {{$comment->user->name}}
+                                    <span class="text-muted float-right">{{$comment->dateAsCarbon->diffForHumans()}}</span>
+                                </div>
+                                {{$comment->message}}
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
             </div>
             @auth()
                 <section class="row comment justify-content-center">
-                    <div class="col-8 mb-4">
+                    <div class="col-10 mb-4">
                         <div class="section-heading mb-3 text-center">Оставить комментарий</div>
                         <form action="{{route('post.comments.store', $post)}}" method="post">
                             @csrf
